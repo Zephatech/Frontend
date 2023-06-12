d7805009f1c8f269252cadb0f12eff9a87341c9a
Ryan Deng <ryan.deng32@gmail.com>
2023-06-11 23:26:52 -0400

    create basic UI for marketplace, layout, login, register, and product details

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
+import './globals.css'
+import MainLayout from './_components/main-layout'
+
+export const metadata = {
+    title: 'UW Trade',
+}
+
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
-            <body>{children}</body>
+        <html lang="en" className="h-full bg-white">
+            <body className="h-full ">
+                <MainLayout>{children}</MainLayout>
+            </body>
         </html>
     )
 }
