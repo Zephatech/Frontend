b3a7f136efe48af7bb67eff0ab26f6c5382d0203
Robert <z596lu@uwaterloo.ca>
2024-03-08 00:15:24 -0500

    adding style change for my listing

--- app/(marketplace)/my-listings/page.tsx
+++ app/(marketplace)/my-listings/page.tsx
@@ -8,6 +8,7 @@ import WithAuth from '@/app/components/withAuth'
 import { deleteProduct } from '@/app/_utils/api/products'
 import { toast } from 'react-toastify'
 import { useRouter } from 'next/navigation'
+import { TrashIcon, PencilIcon } from '@heroicons/react/24/solid'
 
 const getMyListings = async () => {
   const response = await fetch(
@@ -44,7 +45,8 @@ const MyListings = () => {
       setSellListings((prevListings) =>
         prevListings.filter((listing) => listing.id !== id)
       )
-    }else {
+      toast.success('Listing deleted successfully')
+    } else {
       toast.error('Error deleting listing')
     }
   }
@@ -65,28 +67,28 @@ const MyListings = () => {
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
             <table className="min-w-full">
-              <thead className="bg-white">
+              <thead className="bg-gray-200">
                 <tr>
-                  <th className="py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
-                    Image
+                  <th className="py-3 pl-4 pr-3 text-left text-sm font-semibold text-gray-700 sm:pl-3">
+                    Product Image
                   </th>
-                  <th className="px-3 py-2 text-left text-sm font-semibold text-gray-900">
+                  <th className="px-3 py-3 text-left text-sm font-semibold text-gray-900">
                     Title
                   </th>
-                  <th className="px-3 py-2 text-left text-sm font-semibold text-gray-900">
+                  <th className="px-3 py-3 text-left text-sm font-semibold text-gray-900">
                     Category
                   </th>
-                  <th className="relative py-2 px-3">
+                  <th className="relative py-3 px-3">
                     <span className="sr-only">Actions</span>
                   </th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody className="bg-white divide-y divide-gray-300">
                 {sellListings.map((listing) => (
-                  <tr key={listing.id} className="border-t border-gray-200">
-                    <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm sm:pl-0">
+                  <tr key={listing.id} className="hover:bg-gray-100">
+                    <td className="whitespace-nowrap py-3 pl-4 pr-3 text-sm sm:pl-0">
                       <img
-                        className="h-11 w-11 rounded-full"
+                        className="h-12 w-12 rounded-full"
                         src={
                           listing.image === ''
                             ? 'https://png.pngtree.com/png-vector/20190820/ourmid/pngtree-no-image-vector-illustration-isolated-png-image_1694547.jpg'
@@ -95,30 +97,30 @@ const MyListings = () => {
                         alt=""
                       />
                     </td>
-                    <td className="whitespace-nowrap px-3 py-2 text-sm text-gray-900">
-                      <div className="font-medium text-blue-700 underline">
+                    <td className="whitespace-nowrap px-3 py-3 text-sm text-gray-700">
+                      <div className="font-medium text-indigo-600 underline hover:text-indigo-800">
                         <Link href={`/products/${listing.id}`}>
                           {listing.name}
                         </Link>
                       </div>
                     </td>
-                    <td className="whitespace-nowrap px-3 py-2 text-sm text-gray-900">
+                    <td className="whitespace-nowrap px-3 py-3 text-sm text-gray-900">
                       {listing.category}
                     </td>
-                    <td className="flex flex-col px-3 py-2 align-center justify-center h-full w-full">
-                      {/* You can replace the buttons below with your actual actions */}
+                    <td className="flex justify-end items-center space-x-2 px-3 py-3">
                       <button
-                        className="px-2 py-1 bg-blue-500 text-white rounded-md mr-2"
+                        className="p-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700"
+                        onClick={() =>
+                          router.push(`/update-product?id=${listing.id}`)
+                        }
+                      >
+                        <PencilIcon className="h-4 w-4" />
+                      </button>
+                      <button
+                        className="p-2 bg-red-600 text-white rounded-md hover:bg-red-700"
                         onClick={() => handleDelete(listing.id)}
                       >
-                        Delete
-                      </button>
-                      <span style={{ marginTop: '8px' }}></span>
-                      <button
-                        className="px-2 py-1 bg-blue-500 text-white rounded-md mr-2"
-                        onClick={() => router.push(`/update-product?id=${listing.id}`)}
-                      >
-                        Edit
+                        <TrashIcon className="h-4 w-4" />
                       </button>
                     </td>
                   </tr>
